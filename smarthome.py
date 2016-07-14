fa67f82bfb44bec03c153f700535ce18c231cc9f
7iacura <m.curatitoli@gmail.com>
2016-07-14 11:24:27 +0200

    ghmm.org example correctly running ghmm can be splitted in three phases: 1. build hmm from data 2. train 3. viterbi
    with our data:
    1. ok
    2. and 3. error
    
    fuck.

--- smarthome.py
+++ smarthome.py
@@ -102,9 +102,9 @@ def normalize_list(list):
     part = 0
     for c, elem in enumerate(list):
         if c+1 == len(list):
-            list[c] = round(1.00 - part, 9)
+            list[c] = 1.00 - part
         else:
-            list[c] = round(np.divide(float(list[c]), float(tot)), 9)
+            list[c] = np.divide(float(list[c]), float(tot))
             part += list[c]
 
 ### print matrix structure
@@ -114,18 +114,21 @@ def print_matrix(matrix):
 
 ### normalize matrix structure
 def normalize_matrix(matrix):
-    for x, row in enumerate(matrix):
-        tot_in_row = 0
-        for col in row:
-            tot_in_row += col
-        if tot_in_row != 0:
-            part_in_row = 0
-            for y, col in enumerate(row):
-                if x+1 == len(row):
-                    matrix[x][y] = round(1.00 - part_in_row, 9)
-                else:
-                    matrix[x][y] = round(np.divide(float(matrix[x][y]), float(tot_in_row)), 9)
-                    part_in_row += col
+    for row in matrix:
+        normalize_list(row)
+
+    # for x, row in enumerate(matrix):
+    #     tot_in_row = 0
+    #     for col in row:
+    #         tot_in_row += col
+    #     if tot_in_row != 0:
+    #         part_in_row = 0
+    #         for y, col in enumerate(row):
+    #             if y+1 == len(row):
+    #                 matrix[x][y] = round(1.00 - part_in_row, 9)
+    #             else:
+    #                 matrix[x][y] = round(np.divide(float(matrix[x][y]), float(tot_in_row)), 9)
+    #                 part_in_row += matrix[x][y]
 
 ### create csv from lists
 def csv_list(list_names, list_values, file_name):
@@ -221,27 +224,32 @@ def obtain_t_adls(path_file, list_adls, house_name):
                         for y, b in enumerate(list_adls):
                             if b == next_a:
                                 matrix[x][y] += 1
-
     normalize_matrix(matrix)
     print '%s : T(ADLs) calculated' %house_name
-    # csv_square_matrix(list_adls, matrix, house_name+'_T(ADLs)')
     csv_matrix(list_adls, list_adls, matrix, house_name+'_T(ADLs)')
     return matrix
 
 def obtain_list_sens(path_file, house_name):
     ### build list of all possible triple Location-Type-Place
-    list_triple = []
+    ### ad save the order of sensors detections
+    list_sens = []
+    # sens_seq = ''
+    sens_seq = []
     with open(path_file+'.csv', 'rb') as csvfile:
         reader = csv.reader(csvfile, dialect='excel', delimiter='\t')
         for row in reader:
-            triple = '%s %s %s' %(copy.deepcopy(row[2]), copy.deepcopy(row[3]), copy.deepcopy(row[4]))
-            exist_triple = False
-            for t in list_triple:
-                if t == triple:
-                    exist_triple = True
-            if not exist_triple:
-                list_triple.append(triple)
-    return list_triple
+            sens = '%s %s %s' %(copy.deepcopy(row[2]), copy.deepcopy(row[3]), copy.deepcopy(row[4]))
+            exist_sens = False
+            for c, s in enumerate(list_sens):
+                if s == sens:
+                    exist_sens = True
+                    # sens_seq += '%s' %c
+                    sens_seq.append(c)
+            if not exist_sens:
+                list_sens.append(sens)
+                # sens_seq += '%s' %(len(list_sens)-1)
+                sens_seq.append(len(list_sens)-1)
+    return list_sens, sens_seq
 
 ### obtain probability of observations sens from adls
 ### return [o_sens_adls] = [matrix] = matrix with observations probabilities
@@ -283,6 +291,24 @@ def obtain_o_sens_adls(path_adls, list_adls, path_sens, list_sens, house_name):
     csv_matrix(list_adls, list_sens, matrix, house_name+'_O(Sens|ADLs)')
     return matrix
 
+def ghmm_matrix(sigma, A, B, pi, *seqs):
+    m = HMMFromMatrices(sigma, DiscreteDistribution(sigma), A, B, pi)
+    print '\n', m
+    return m
+
+def ghmm_train(m, train_seq):
+    m.baumWelch(train_seq)
+    print 'trained with baumWelch method\n'
+
+def ghmm_viterbi(sigma, m, test_seq):
+    vt = m.viterbi(test_seq)
+    print 'analyzed >test_seq< with viterbi algorithm\n', vt
+
+    my_seq = EmissionSequence(sigma, [1] * 20 + [6] * 10 + [1] * 40)
+    vm = m.viterbi(my_seq)
+    print
+    print 'analyzed >my_seq< with viterbi algorithm\n', vm
+
 
 def project():
 
@@ -301,7 +327,7 @@ def project():
     dataset = [test1]
 
     for house in dataset:
-        print '\n'
+        print 
         for path_file in house:
             ### check the correctness of file
             if ('ADLs' in path_file) or ('Sensors' in path_file):
@@ -314,52 +340,45 @@ def project():
         temp = obtain_p_adls(path_adls, house_name)
         list_adls = temp[0]
         p_adls = temp[1]
-
         t_adls = obtain_t_adls(path_adls, list_adls, house_name)
 
-        list_sens = obtain_list_sens(path_sens, house_name)
+        temp = obtain_list_sens(path_sens, house_name)
+        list_sens = temp[0]
+        sens_seq = temp[1]
         o_sens_adls = obtain_o_sens_adls(path_adls, list_adls, path_sens, list_sens, house_name)
 
-        print '\n++++++++++++++++++++++++++++++++++++++++++++++++++'
-        sigma = IntegerRange(1,7)
-        print 'sigma: %s' %sigma
-        print 'len(sigma): %s' %len(sigma)
-        aigma = IntegerRange(1,len(list_adls)+1)
-        print 'aigma: %s' %aigma
-        print 'len(aigma): %s' %len(aigma)
-        print '++++++++++++++++++++++++++++++++++++++++++++++++++'
-        A = [[0.9, 0.1], [0.3, 0.7]]
-        AM = matrix(A)
-        print 'size(A): %s' %str(AM.shape)
-        TAM = matrix(t_adls)
-        print 'size(t_adls): %s' %str(TAM.shape)
-        print_matrix(t_adls)
-        print '++++++++++++++++++++++++++++++++++++++++++++++++++'
-        efair = [1.0 / 6] * 6
-        eloaded = [3.0 / 13, 3.0 / 13, 2.0 / 13, 2.0 / 13, 2.0 / 13, 1.0 / 13]
-        B = [efair, eloaded]
-        BM = matrix(B)
-        print 'size(B): %s' %str(BM.shape)
-        OSAM = matrix(o_sens_adls)
-        print 'size(o_sens_adls): %s' %str(OSAM.shape)
-        print_matrix(o_sens_adls)
-        print '++++++++++++++++++++++++++++++++++++++++++++++++++'
-        pi = [0.5] * 2
-        piM = matrix(pi)
-        print 'size(pi): %s' %str(piM.shape)
-        PAM = matrix(p_adls)
-        print 'size(p_adls): %s' %str(PAM.shape)
-        print p_adls
-        print '++++++++++++++++++++++++++++++++++++++++++++++++++'
-        
-        m = HMMFromMatrices(sigma, DiscreteDistribution(sigma), A, B, pi)
-        print m
+        e = IntegerRange(0, len(list_sens)) ### emission domain
+        # print sens_seq
+        # real_seq = [1, 2, 3, 4, 5, 6, 7, 8, 6, 7, 9, 10, 3, 11, 3, 4, 12, 12, 6, 11, 4]
+        train_sens_seq = EmissionSequence(e, sens_seq)
+        # test = [3, 8, 2, 3, 4, 5, 2, 2, 7, 0, 11, 2]
+        # test_sens_seq = EmissionSequence(e, test)
 
-        n = HMMFromMatrices(aigma, DiscreteDistribution(aigma), t_adls, o_sens_adls, p_adls)
-        print n
+        mm = ghmm_matrix(e, t_adls, o_sens_adls, p_adls)
+        # ghmm_train(mm, train_sens_seq)
+        # ghmm_viterbi(e, mm, test_sens_seq)
 
+    ### //////////////////////////////////////////////
+    ### example from ghmm.org
+    print '//////////////////////////////////////////////'
+    print 'example from ghmm.org'
 
-        print '\n------------------------------------------------------------'
+    sigma = IntegerRange(1, 7)
+    A = [[0.9, 0.1], [0.3, 0.7]]
+    efair = [1.0 / 6] * 6
+    eloaded = [3.0 / 13, 3.0 / 13, 2.0 / 13, 2.0 / 13, 2.0 / 13, 1.0 / 13]
+    B = [efair, eloaded]
+    pi = [0.5] * 2
+    tr = [1, 6, 2, 5, 5, 4, 5, 1, 2, 1, 3, 6, 6, 3, 2, 1, 4, 4, 1, 1, 4, 2, 1, 1, 6, 3, 3, 2, 1, 4, 4, 3, 3, 5, 3, 3, 3, 3, 4, 3, 1, 5, 4, 1, 4, 5, 1, 1, 3, 4, 3, 5, 5, 1, 5, 2, 1, 5, 3, 6, 3, 6, 5, 6, 5, 3, 3, 1, 2, 6, 3, 3, 2, 2, 5, 4, 1, 5, 6, 3, 3, 5, 1, 5, 2, 3, 1, 1, 1, 5, 6, 4, 5, 5, 1, 6, 2, 6, 5, 3, 1, 1, 3, 3, 1, 1, 2, 5, 2, 3, 2, 4, 1, 5, 5, 5, 4, 6, 5, 6, 3, 1, 6, 1, 5, 4, 3, 1, 3, 1, 2, 6, 3, 5, 2, 1, 3, 6, 4, 4, 4, 4, 3, 5, 1, 6, 4, 4, 3, 5, 1, 5, 5, 5, 5, 6, 5, 1, 6, 1, 1, 4, 1, 4, 2, 6, 6, 2, 5, 4, 5, 5, 4, 3, 3, 6, 5, 4, 1, 5, 3, 3, 3, 2, 5, 6, 2, 3, 3, 5, 1, 3, 4, 5, 1, 6, 3, 2, 4, 5, 2, 2, 5, 1, 4, 5, 1, 5, 6, 5, 3, 4, 6, 1, 3, 2, 4, 6, 6, 3, 1, 6, 5, 2, 5, 4, 4, 4, 4, 2, 6, 3, 3, 2, 1, 1, 6, 5, 3, 3, 3, 4, 5, 6, 1, 6, 2, 3, 1, 4, 6, 5, 3, 1, 2, 4, 6, 2, 6, 2, 1, 2, 6, 5, 6, 1, 4, 4, 1, 5, 5, 5, 3, 4, 4, 5, 2, 2, 5, 1, 2, 1, 3, 2, 3, 3, 3, 5, 3, 5, 2, 3, 5, 5, 5, 2, 6, 2, 5, 6, 4, 5, 4, 4, 3, 3, 6, 2, 3, 2, 2, 6, 1, 1, 1, 4, 1, 2, 6, 1, 4, 5, 2, 6, 2, 6, 6, 3, 2, 4, 2, 2, 2, 3, 3, 4, 4, 1, 1, 1, 5, 3, 4, 3, 5, 3, 3, 3, 4, 6, 1, 1, 6, 4, 2, 4, 6, 4, 1, 5, 1, 4, 5, 5, 4, 6, 1, 3, 5, 1, 5, 3, 1, 6, 6, 3, 3, 2, 4, 4, 3, 1, 2, 5, 3, 5, 4, 5, 3, 2, 2, 6, 4, 4, 2, 6, 4, 6, 1, 4, 6, 3, 3, 4, 6, 3, 2, 3, 5, 3, 5, 4, 2, 2, 2, 1, 4, 1, 5, 1, 1, 1, 6, 6, 3, 4, 4, 5, 1, 2, 4, 3, 3, 1, 1, 5, 6, 1, 3, 4, 6, 2, 4, 5, 2, 1, 1, 2, 6, 5, 4, 3, 5, 4, 5, 2, 5, 1, 1, 5, 4, 3, 6, 5, 3, 6, 4, 2, 3, 5, 1, 4, 6, 1, 2, 6, 1, 3, 5, 2, 1, 2, 6, 1, 2, 5, 4, 4, 5, 5, 1, 4, 3, 1, 6, 5, 4, 3, 4, 2, 4, 2, 6, 6, 3, 3, 6, 1, 5, 6, 4, 1, 3, 2, 2, 1, 3, 4, 6, 3, 5, 5, 1, 5, 5, 2, 5, 3, 3, 1, 3, 4, 2, 1, 1, 2, 1, 5, 1, 5, 4, 2, 6, 3, 5, 6, 6, 2, 2, 5, 6, 1, 5, 3, 1, 2, 1, 2, 3, 4, 5, 5, 4, 3, 3, 3, 4, 6, 2, 5, 2, 5, 5, 3, 1, 4, 3, 6, 2, 5, 6, 3, 4, 2, 6, 2, 5, 1, 3, 1, 2, 2, 1, 4, 1, 6, 1, 4, 5, 5, 5, 2, 3, 2, 5, 1, 6, 2, 5, 5, 1, 4, 4, 6, 1, 3, 1, 1, 4, 3, 1, 2, 1, 1, 1, 4, 2, 2, 2, 1, 2, 3, 5, 5, 4, 4, 4, 6, 3, 6, 4, 3, 5, 2, 4, 4, 5, 6, 2, 6, 5, 5, 3, 1, 5, 4, 4, 1, 5, 3, 3, 1, 2, 6, 3, 6, 6, 6, 6, 1, 1, 3, 2, 5, 6, 4, 3, 4, 6, 5, 2, 2, 1, 2, 5, 5, 6, 1, 5, 2, 5, 6, 2, 4, 5, 4, 2, 5, 5, 4, 3, 4, 1, 6, 1, 1, 6, 2, 2, 1, 3, 4, 3, 1, 1, 6, 1, 1, 1, 4, 4, 4, 1, 5, 6, 4, 4, 2, 1, 4, 6, 6, 2, 3, 6, 2, 4, 3, 4, 3, 3, 3, 4, 1, 2, 4, 3, 3, 6, 2, 4, 1, 1, 2, 5, 4, 6, 3, 1, 2, 1, 3, 2, 1, 3, 6, 2, 2, 2, 5, 1, 1, 6, 6, 6, 1, 4, 2, 6, 2, 3, 3, 2, 4, 1, 2, 5, 2, 6, 3, 5, 6, 2, 1, 3, 6, 5, 1, 4, 1, 4, 6, 4, 4, 4, 2, 2, 3, 6, 4, 6, 5, 4, 2, 2, 5, 5, 3, 4, 2, 3, 3, 6, 6, 3, 1, 5, 5, 5, 6, 4, 2, 6, 5, 4, 5, 6, 3, 1, 1, 5, 4, 5, 5, 3, 5, 3, 2, 2, 1, 3, 3, 6, 5, 3, 5, 1, 2, 5, 2, 4, 1, 5, 2, 1, 2, 5, 3, 1, 3, 4, 4, 1, 5, 1, 3, 1, 2, 6, 3, 6, 1, 3, 5, 2, 6, 6, 5, 2, 3, 6, 3, 3, 4, 5, 1, 6, 4, 2, 4, 1, 4, 3, 3, 4, 2, 3, 1, 6, 2, 4, 3, 5, 2, 2, 4, 5, 1, 3, 2, 2, 1, 6, 3, 2, 1, 2, 1, 5, 4, 2, 1, 4, 5, 5, 3, 4, 4, 6, 5, 3, 6, 5, 3, 6, 5, 6, 3, 3, 5, 3, 3, 2, 4, 2, 6, 3, 2, 6, 5, 5, 5, 3, 3, 1, 2, 5, 1, 3, 6, 3, 3, 5, 1, 4, 4, 4, 2, 3, 4, 2, 6, 1, 6, 1, 4, 3, 2, 1, 3, 5, 4, 6, 5, 6, 6, 6, 4, 2, 4, 3, 5, 1, 1, 1, 5, 4, 6, 2, 2, 4, 3, 2, 1, 6, 2, 1, 6, 1, 6, 5, 1, 6, 4, 3, 6, 5, 4, 4, 1, 3, 4, 3, 1, 6, 4, 4, 2, 1, 2, 5, 5, 1, 4, 3, 2, 3, 2, 1, 4, 6, 3, 6, 6, 1, 5, 3, 6, 1, 3, 6, 6, 5, 3, 2, 4, 6, 2, 4, 4, 4, 2, 4, 4, 3, 6, 5, 3, 4, 1, 3, 3, 1, 6, 1, 4, 1, 6, 3, 5, 2, 1, 5, 4, 2, 4, 6, 1, 4, 3, 1, 1, 3, 2, 5, 5, 1, 1, 5, 3, 4, 3, 1, 3, 2, 5, 6, 2, 1, 3, 2, 6, 3, 6, 4, 4, 3, 5, 3, 2, 5, 2, 3, 2, 4, 2, 6, 2, 4, 6, 5, 5, 2, 3, 5, 6, 4, 3, 1, 3, 3, 2, 2, 2, 3, 6, 1, 6, 3, 1, 6, 3, 1, 3, 1, 1, 1, 4, 3, 1, 5, 4, 6, 2, 1, 6, 2, 2, 2, 1, 5, 5, 1, 2, 5, 5, 2, 5, 2, 4, 5, 1, 5, 5, 6, 6, 5, 3, 5, 6, 2, 5, 5, 5, 1, 2, 2, 4, 2, 4, 5, 5, 1, 4, 1, 5, 3, 5, 1, 4, 2, 1, 2, 2, 2, 4, 4, 4, 4, 2, 1, 1, 4, 5, 4, 1, 2, 2, 3, 5, 6, 4, 1, 1, 1, 3, 6, 1, 2, 4, 3, 2, 3, 2, 3, 6, 6, 3, 4, 4, 4, 6, 6, 2, 6, 6, 3, 2, 3, 5, 1, 2, 4, 1, 3, 5, 5, 1, 2, 5, 1, 5, 6, 2, 6, 2, 1, 1, 4, 4, 2, 1, 5, 2, 3, 4, 4, 2, 5, 2, 5, 5, 5, 3, 1, 4, 6, 6, 5, 5, 1, 3, 3, 6, 5, 6, 2, 1, 1, 1, 5, 4, 3, 1, 1, 2, 1, 3, 1, 6, 1, 5, 1, 2, 6, 1, 2, 2, 2, 5, 1, 2, 6, 5, 2, 2, 1, 3, 6, 2, 6, 1, 1, 6, 3, 5, 2, 6, 3, 1, 1, 4, 3, 5, 2, 3, 2, 4, 5, 1, 2, 5, 5, 3, 1, 4, 4, 5, 5, 5, 3, 2, 1, 3, 1, 3, 2, 2, 5, 6, 5, 2, 2, 6, 4, 1, 4, 2, 3, 5, 2, 5, 5, 6, 4, 6, 4, 3, 5, 4, 3, 5, 2, 6, 5, 5, 5, 1, 4, 6, 5, 3, 5, 5, 4, 5, 4, 4, 3, 1, 6, 4, 1, 5, 5, 6, 3, 2, 4, 3, 4, 4, 4, 5, 3, 3, 4, 2, 5, 3, 6, 1, 4, 5, 5, 6, 1, 4, 1, 1, 2, 2, 1, 3, 5, 1, 1, 2, 1, 2, 1, 1, 2, 3, 1, 3, 5, 5, 6, 4, 3, 5, 6, 1, 3, 4, 5, 6, 3, 1, 5, 4, 2, 3, 2, 6, 4, 6, 6, 6, 4, 1, 5, 1, 6, 4, 6, 6, 1, 3, 1, 4, 6, 2, 1, 4, 2, 4, 4, 4, 6, 6, 1, 6, 3, 6, 5, 3, 5, 3, 5, 6, 6, 1, 6, 2, 2, 1, 6, 1, 1, 1, 6, 3, 3, 6, 5, 1, 1, 4, 4, 2, 6, 5, 3, 4, 6, 5, 4, 2, 2, 1, 5, 3, 5, 5, 5, 4, 4, 6, 4, 4, 3, 1, 1, 6, 2, 3, 3, 3, 6, 5, 2, 1, 3, 1, 4, 4, 2, 3, 1, 2, 6, 1, 6, 4, 4, 3, 1, 4, 6, 1, 5, 4, 5, 3, 2, 2, 5, 5, 3, 6, 1, 2, 5, 5, 5, 4, 4, 4, 5, 1, 2, 4, 1, 6, 4, 3, 4, 4, 2, 4, 3, 4, 3, 5, 2, 3, 2, 3, 6, 6, 5, 2, 5, 5, 1, 4, 3, 5, 6, 5, 5, 1, 5, 3, 1, 3, 2, 2, 5, 6, 2, 6, 6, 4, 5, 2, 3, 1, 5, 5, 2, 2, 6, 5, 2, 5, 5, 3, 4, 6, 2, 1, 5, 6, 3, 2, 3, 2, 6, 6, 1, 4, 6, 5, 6, 6, 6, 5, 6, 6, 3, 6, 6, 4, 5, 5, 4, 6, 5, 2, 6, 6, 3, 3, 3, 3, 6, 5, 5, 6, 1, 3, 6, 1, 1, 1, 3, 4, 4, 4, 1, 4, 6, 6, 1, 5, 4, 1, 3, 2, 4, 5, 3, 6, 2, 3, 3, 3, 4, 5, 3, 2, 4, 4, 2, 3, 6, 4, 6, 6, 1, 2, 1, 2, 3, 1, 6, 5, 4, 4, 4, 5, 6, 6, 1, 5, 4, 1, 4, 3, 2, 1, 3, 2, 3, 1, 6, 5, 2, 3, 6, 6, 1, 1, 5, 2, 2, 1, 3, 2, 5, 2, 6, 5, 4, 5, 2, 2, 1, 6, 3, 1, 5, 1, 6, 4, 6, 3, 3, 1, 1, 4, 4, 6, 4, 2, 1, 6, 2, 4, 3, 4, 4, 5, 5, 2, 1, 4, 3, 5, 6, 6, 6, 2, 5, 6, 2, 3, 2, 4, 5, 6, 3, 3, 5, 4, 1, 1, 1, 2, 5, 6, 1, 4, 5, 3, 4, 2, 1, 6, 6, 4, 2, 6, 4, 3, 1, 2, 4, 2, 4, 6, 5, 6, 5, 6, 4, 4, 1, 2, 4, 6, 6, 1, 5, 2, 3, 2, 1, 5, 4, 1, 4, 4, 3, 6, 4, 6, 1, 6, 6, 2, 3, 5, 4, 3, 2, 1, 4, 3, 2, 2, 4, 5, 2, 1, 6, 1, 6, 1, 5, 1, 3, 1, 1, 6, 5, 6, 6, 6, 4, 6, 3, 4, 1, 4, 1, 1, 6, 3, 1, 3, 3, 4, 5, 2, 5, 4, 3, 1, 5, 5, 6, 6, 6, 5, 3, 6, 2, 4, 5, 6, 2, 3, 2, 3, 1, 3, 6, 4, 4, 5, 5, 5, 3, 1, 1, 6, 1, 4, 1, 2, 3, 3, 2, 3, 4, 1]
+    train_seq = EmissionSequence(sigma, tr)
+    test_seq = EmissionSequence(sigma, [4, 6, 1, 1, 5, 2, 5, 3, 4, 2, 1, 6, 1, 5, 5, 5, 6, 2, 4, 3, 4, 3, 4, 1, 3, 4, 2, 2, 3, 3, 2, 6, 6, 3, 6, 4, 1, 4, 4, 4, 6, 2, 1, 1, 2, 2, 2, 3, 5, 1, 2, 1, 4, 2, 6, 1, 6, 4, 4, 1, 1, 4, 6, 5, 1, 2, 5, 6, 3, 5, 1, 1, 2, 2, 1, 1, 5, 4, 6, 6, 3, 5, 4, 4, 4, 3, 3, 6, 6, 2, 1, 2, 1, 3, 2, 6, 2, 4, 2, 4])
+    test_vit = []
+
+    m = ghmm_matrix(sigma, A, B, pi)
+    ghmm_train(m, train_seq)
+    ghmm_viterbi(sigma, m, test_seq)
+
+    ### //////////////////////////////////////////////
 
 if __name__ == '__main__':
     project()
